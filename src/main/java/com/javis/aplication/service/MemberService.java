7cab0417808a49cfef4aa983ba7cec965487ed03
wjdwnsdnjs13 <67488973+wjdwnsdnjs13@users.noreply.github.com>
2024-08-21 07:02:58 +0900

    refactor : 이메일 중복 검사 구현

--- src/main/java/com/javis/aplication/service/MemberService.java
+++ src/main/java/com/javis/aplication/service/MemberService.java
@@ -2,6 +2,7 @@ package com.javis.aplication.service;
 
 import com.javis.aplication.dto.request.LoginRequestDto;
 import com.javis.aplication.dto.request.RegisterRequestDto;
+import com.javis.aplication.entity.MemberEntity;
 import com.javis.aplication.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -12,6 +13,7 @@ public class MemberService {
     private final MemberRepository memberRepository;
 
     public Long save(RegisterRequestDto registerRequestDto) {
+        memberRepository.findByEmail(registerRequestDto.email());
         return memberRepository.save(registerRequestDto.toMemberEntity()).getId();
     }
 
